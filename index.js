61900ff18f89de7031b7c44a389e378d0764372d
Ebuka <ebukaopara180@gmail.com>
2025-06-01 18:24:05 +0100

    updated

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+function rollDice() {
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  var randomDiceImage1 = "images/dice" + randomNumber1 + ".png";
+  document.querySelector(".img1").setAttribute("src", randomDiceImage1);
 
-var randomDiceImage = "dice" + randomNumber1 + ".png";
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+  var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
+  document.querySelector(".img2").setAttribute("src", randomDiceImage2);
 
-var randomImageSource = "images/" + randomDiceImage;
+  var title = document.querySelector("h1");
 
-var image1 = document.querySelectorAll("img")[0];
-
-image1.setAttribute("src", randomImageSource);
-
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
-
-document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
-
-if (randomNumber1 > randomNumber2){
-    document.querySelector("h1").innerHTML = "🚩player 1 wins!";
+  if (randomNumber1 > randomNumber2) {
+    title.innerHTML = "🚩 Player 1 Wins!";
+  } else if (randomNumber2 > randomNumber1) {
+    title.innerHTML = "Player 2 Wins! 🚩";
+  } else {
+    title.innerHTML = "Draw! Try Again";
+  }
 }
-else if (randomNumber2 > randomNumber1){
-    document.querySelector("h1").innerHTML = "player 2 wins🚩";
-}
-else {
-    document.querySelector("h1").innerHTML = "Draw! Play Again";
-}
+
+document.getElementById("rollButton").addEventListener("click", rollDice);
